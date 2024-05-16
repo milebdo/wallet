bc6831af7d8608aa5991eeb9a8cae42ecfa418d5
Ismail <ismail@smartm2m.co.kr>
2024-05-16 16:55:24 +0700

    revise path

--- network/base/organizations/ccp-generate.sh
+++ network/base/organizations/ccp-generate.sh
@@ -12,7 +12,7 @@ function json_ccp {
         -e "s/\${CAPORT}/$3/" \
         -e "s#\${PEERPEM}#$PP#" \
         -e "s#\${CAPEM}#$CP#" \
-        ccp-template.json
+        ${PWD}/base/organizations/ccp-template.json
 }
 
 function yaml_ccp {
@@ -23,7 +23,7 @@ function yaml_ccp {
         -e "s/\${CAPORT}/$3/" \
         -e "s#\${PEERPEM}#$PP#" \
         -e "s#\${CAPEM}#$CP#" \
-        ccp-template.yaml | sed -e $'s/\\\\n/\\\n          /g'
+        ${PWD}/base/organizations/ccp-template.yaml | sed -e $'s/\\\\n/\\\n          /g'
 }
 
 ORG=1
